f57f164fc8356f6a888a3f661f14b790fc296aab
ekowahyuutomo <ekowahyuutomocivil@gmail.com>
2020-05-02 20:56:47 +0700

    UTS Eko Wahyu Utomo

---
+++ Uts_EkoWahyuUtomo_JualBeliHp/app/src/main/java/com/example/uts_ekowahyuutomo_jualbelihp/Server/BuildConfig.java
@@ -0,0 +1,10 @@
+package com.example.uts_ekowahyuutomo_jualbelihp.Server;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "com.indra.jualbeliti17d4";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+}
